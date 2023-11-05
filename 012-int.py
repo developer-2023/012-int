9de33cb7f29cb19aa7e5927c62a885d4e5664491
developer-2023 <132809345+developer-2023@users.noreply.github.com>
2023-11-05 21:11:04 +0100

    Initial commit

---
+++ 012-int.py
@@ -0,0 +1,25 @@
+# First version
+
+# Enter two strings
+x = input("What's x? ")
+y = input("What's y? ")
+
+# Calculate sum of of two integers converted from strings
+z = int(x) + int(y)
+
+# Show result
+print(z)
+
+# Second version
+
+# Enter two integer strings and convert them into integers
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+# Calculate sum of them and show result
+print(x + y)
+
+# Third version
+
+# Enter two strings, convert them into integers, sum them and show the result
+print(int(input("What's x? ")) + int(input("What's y? ")))
